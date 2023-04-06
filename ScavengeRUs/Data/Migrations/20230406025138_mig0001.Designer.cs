889bc75c5240ce6e4c8530f7c1a2ca273cddd04b
thomas <thomas.foreman17@gmail.com>
2023-04-06 09:39:38 -0400

    Added the creationdate to the hunt model and update hunt.

--- ScavengeRUs/Data/Migrations/20230406025138_mig0001.Designer.cs
+++ ScavengeRUs/Data/Migrations/20230406025138_mig0001.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScavengeRUs.Data;
 
@@ -10,9 +11,10 @@ using ScavengeRUs.Data;
 namespace ScavengeRUs.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230406025138_mig0001")]
+    partial class mig0001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
@@ -259,6 +261,7 @@ namespace ScavengeRUs.Data.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<DateTime>("CreationDate")
+                        .ValueGeneratedOnAddOrUpdate()
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("EndDate")
