b998e15ab1b93c89ef7bf55c512b1d756a8a7e53
Daniel Tucker <TUCKERD1@ETSU.EDU>
2023-11-25 09:18:33 -0500

    Sanitized Inputs on create user page and fixed the crash when entering an invalid access code

--- ScavengeRUs/Models/Entities/HuntLocation.cs
+++ ScavengeRUs/Models/Entities/HuntLocation.cs
@@ -3,8 +3,10 @@
     public class HuntLocation
     {
         /// <summary>
-        /// This is the weak entity for the many to many relationship between Hunt and Location(tasks)
+        /// This is the weak entity for the many to many relationship between Hunt and 
+        /// Location(tasks)
         /// </summary>
+        
         public int Id { get; set; }
         public int HuntId { get; set; } //Foreign key
         public Hunt? Hunt { get; set; } //Navigation property
